57dd95a485902a53581b712e04031e5880949174
mahak540 <162689029+mahak540@users.noreply.github.com>
2025-07-18 17:29:24 +0530

    Update verticaltreetraversal.cpp

--- verticaltreetraversal.cpp
+++ verticaltreetraversal.cpp
@@ -1,24 +1,31 @@
-void DFS(Node* root,int hd,int &mn,unordered_map<int,vector<int>>&mp){
-      if(root==NULL)
-      return;
-      mp[hd].push_back(root->data);
-      mn=min(mn,hd);
-      
-      DFS(root->left,hd-1,mn,mp);
-      DFS(root->right,hd+1,mn,mp);
-  }
-    vector<vector<int>> verticalOrder(Node *root) {
+  vector<vector<int>> verticalOrder(Node *root) {
         // Your code here
-        unordered_map<int,vector<int>>mp;
-        int mn=0;
-        DFS(root,0,mn,mp);
-        vector<vector<int>>res;
-        int hd=mn;
-        while(mp.find(hd)!=mp.end())
+        if(!root)
+        return {};
+        map<int,vector<int>>mp;
+        queue<pair<Node*,int>>q;
+        q.push({root,0});
+        
+        while(!q.empty())
         {
-            res.push_back(mp[hd]);
-            hd++;
+            pair<Node*,int>p=q.front();
+            Node* node=p.first;
+            int hd=p.second;
+            q.pop();
+            mp[hd].push_back(node->data);
+            
+            if(node->left!=NULL)
+            q.push({node->left,hd-1});
+             if(node->right!=NULL)
+            q.push({node->right,hd+1});
+            
+        }
+        vector<vector<int>>res;
+        for(auto it=mp.begin();it!=mp.end();++it)
+        {
+            res.push_back(it->second);
         }
         return res;
+        
         
     }
